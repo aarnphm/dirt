97c022927e9150f528858306111306bdd16612a7
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-06-24 23:02:27 -0400

    chore: add hazard
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- yolo/service.py
+++ yolo/service.py
@@ -1,8 +1,7 @@
-from typing import List
-  
+import logging
 import bentoml
-import numpy as np
 import torch
+import numpy as np
 from bentoml.io import JSON
 
 runner = bentoml.pytorch.get("yolov5:latest").to_runner()
@@ -10,10 +9,8 @@ service = bentoml.Service("yolov5", runners=[runner])
 
 
 @service.api(input=JSON(), output=JSON())
-def predict(input_dict):
-    filename = input_dict['filename']
-    img = input_dict['image_content']
+async def predict(input_dict):
+    img = input_dict["image_content"]
     input_img = torch.from_numpy(np.asarray(img[0]))
-    predictions = runner.run_batch(input_img)
+    predictions = await runner.async_run(input_img)
     return predictions
-    
