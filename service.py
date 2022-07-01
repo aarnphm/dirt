65a0237f26811126d7c2143ca8f2fefcc7a9e236
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-06-30 19:37:04 -0700

    chore: remove tf compiling for now
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- service.py
+++ service.py
@@ -1,3 +1,4 @@
+"""service definition"""
 import bentoml
 import numpy as np
 from bentoml.io import NumpyNdarray
@@ -9,4 +10,5 @@ svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
 
 @svc.api(input=NumpyNdarray(), output=NumpyNdarray())
 def classify(input_series: np.ndarray) -> np.ndarray:
-    return iris_clf_runner.predict.run(input_series)
+    """classify a series of numpy array."""
+    return iris_clf_runner.run(input_series)
