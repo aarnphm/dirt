528586f12683ab39941b24e6fd0e98c568b789f1
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-07-09 23:01:57 -0700

    chore: update mnist gen script
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- service.py
+++ service.py
@@ -1,17 +1,10 @@
 from __future__ import annotations
 
-import logging
-
 import typing as t
 import numpy as np
 import bentoml
-from bentoml.io import Text
 from bentoml.io import NumpyNdarray
-from starlette.responses import PlainTextResponse
-from starlette.middleware.base import BaseHTTPMiddleware
 
-bentoml_logger = logging.getLogger("bentoml")
-bentoml_logger.setLevel(logging.DEBUG)
 
 iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
 svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
@@ -23,14 +16,17 @@ def classify(input_series: np.ndarray[t.Any]) -> np.ndarray[t.Any]:
     return iris_clf_runner.run(input_series)
 
 
-class CustomReadyzMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        if request.url == "http://127.0.0.1:3000/readyz":
-            return PlainTextResponse("Not ready", status_code=503)
-        return await call_next(request)
+# from starlette.responses import PlainTextResponse
+# from starlette.middleware.base import BaseHTTPMiddleware
+
+# class CustomReadyzMiddleware(BaseHTTPMiddleware):
+#     async def dispatch(self, request, call_next):
+#         if request.url == "http://127.0.0.1:3000/readyz":
+#             return PlainTextResponse("Not ready", status_code=503)
+#         return await call_next(request)
 
 
-svc.add_asgi_middleware(CustomReadyzMiddleware)
+# svc.add_asgi_middleware(CustomReadyzMiddleware)
 
 
 # fast_runner = bentoml.fastai.get("iris_fai:latest").to_runner()
